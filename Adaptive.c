fa7c2c7a09ab2a541a93da499f11a2ea4b967876
Pawel Kowalski <pawel.kowalski@bristol.ac.uk>
2016-03-04 16:21:43 +0000

    broken and working again, down to 52 cycles at o3 using pragmas;better performance obtained using simple intrinsics such as mpy's rather than dotp due to loop unrolling

--- Adaptive.c
+++ Adaptive.c
@@ -1,14 +1,21 @@
 short h[8]={0,0,0,0,0,0,0,0};
 short X[8]={0,0,0,0,0,0,0,0};
 int Y=0;
+long long Y_long;
 int E;
 
 //buffers for optimized code
+#pragma DATA_ALIGN(h_opt,8)
 short h_opt[8]={0,0,0,0,0,0,0,0};
+#pragma DATA_ALIGN(X_opt,8)
 short X_opt[8]={0,0,0,0,0,0,0,0};
 
-int *X_opt32 = (int*) X_opt;
-int *h_opt32 = (int*) h_opt;
+
+//int *X_opt32 = (int*) X_opt;
+//int *h_opt32 = (int*) h_opt;
+//
+//double *X_opt64 = (double*) X_opt;
+//double *h_opt64 = (double*) h_opt;
 //end
 LMS_isrNoOpt (short newvalue)
 {
@@ -58,50 +65,72 @@ LMS_isrOpt (short D)
 	#define _beta 0x00000174
 #endif
 
+
     int i;
-    short temp_out;
 	short BETA_E;
+	short temp_out;
+//	int BETA_E_PACK;
 	X_opt[0] = D;
 	// D = X[0];
 	Y=0;
- 	//int Y_temp=0;
+//	double h_delta_32;
+	_nassert((int) h_opt % 8 == 0);
+	_nassert((int) X_opt % 8 == 0);
+//Computation 1: loop1 using _dotp2 intrinsic
 
-// What is the purpose of all the register shifts?
-//debug area
-// 	int y_temp_storage[8];
-// 	int y_storage[4];
- 	//Computation 1: loop 1 - Accumulation - could be optimized (dotp or similar)
-// 	 	for(i=0;i<_N;i++){
- //	 		Y_temp = Y_temp + (_mpy(h_opt[i],X_opt[i])<<1) ;
- //	 	}
+//#pragma MUST_ITERATE(4,4,4)
+//#pragma UNROLL(2)
+//	for(i=0;i<(_N/2);i=i+1){
+//		Y = Y +  _dotp2( h_opt32[i], X_opt32[i] ) ;
+//	}
 
-//end
 
-//Computation 2: loop1 using _dotp2 intrinsic
-	for(i=0;i<(_N/2);i=i+1){
-		//Y = Y + ((_dotp2(h_opt32[i],X_opt32[i]))<<1) ;
-		Y = Y +  _dotp2( h_opt32[i], X_opt32[i] ) ;
+//	for(i=0;i<(_N/4);i=i+1){
+//		Y = Y +  _dotp4h( h_opt64[i], X_opt64[i] ) ;
+//	}
+#pragma MUST_ITERATE(8,,8)
+#pragma UNROLL(8)
+	for(i=0;i<_N;i++)
+	{
+		Y = Y + (_mpy(h_opt[i],X_opt[i])) ;
 	}
-//if(Y_temp!=Y){
-//	printf("wtf");
-
-//}
+	temp_out =  (short)(Y>>15);
+	//	Y_short= (short)(Y>>15);
 
 
 //Computation 2: calculation of E and BETA_E
 		E = D -(short) (Y>>15);
-		BETA_E =(short)((_mpy(_beta,E)) >>15);
-//COmputation 3: loop 2 - filtering
+		BETA_E = (short)((_mpy(_beta,E)) >>15);
+// 		BETA_E_PACK= _pack2(BETA_E,BETA_E);
+
+
+//Computation 3: loop 2 - filtering
+#pragma MUST_ITERATE(8,,8)
+#pragma UNROLL(8)
 	for(i=_N-1;i>=0;i--)
 	{
-		//use a dotp type intrinsic?
 		h_opt[i] = h_opt[i] +((_mpy(BETA_E,X_opt[i])) >> 15);
-
 		//take out of the loop and use a memory shift?
 		X_opt[i]=X_opt[i-1];
 	}
 
-	temp_out =  (short)(Y>>15);
+
+
+//#pragma MUST_ITERATE(4,,4)
+//#pragma UNROLL(4)
+//	for(i=(_N/2)-1;i>=0;i--)
+//	{
+//		h_delta_32 = _mpy2(BETA_E_PACK, X_opt32[i]);
+//		h_opt32[i]=_add2(h_opt32[i], _rpack2(_hi(h_delta_32),_lo(h_delta_32)));
+//
+//	//	h_opt[i] = h_opt[i] +((_mpy(BETA_E,X_opt[i])) >> 15);
+//		//take out of the loop and use a memory shift?
+//
+//		//shuffle
+//		X_opt32[i]=_packlh2(X_opt32[i],X_opt32[i-1]);
+//	}
+
+
 
 	return(temp_out);
 
